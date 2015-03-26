62a99c2ade0687f35f9c44c3fbbbc932ad0992af
Linus Oberg <linus.oberg@gmail.com>
2015-03-26 13:45:50 +0100

    gcm replies

--- app/src/main/java/huka/com/repli/CameraFragment.java
+++ app/src/main/java/huka/com/repli/CameraFragment.java
@@ -132,11 +132,10 @@ public class CameraFragment extends android.support.v4.app.Fragment {
             String url = params[0];
             String account = params[1];
 //            String url = "http://"+LoginActivity.LOCALHOST_IP2+":8080/_ah/img/"+blobKey;
-            mDataset.clear();
+           // mDataset.clear();
             ReplyInfo replyInfo = new ReplyInfo(account);
             replyInfo.setImage(url);
             replyInfo.setThumbnail(url);
-
             mDataset.add(replyInfo);
 
             return null;
